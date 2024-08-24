a511f69c6180d3c74dd5c9597d91624b343aced5
Milica Bićanin <mbicanin99f@gmail.com>
2024-08-24 19:10:47 +0200

    Add app component and config for application

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { MainComponent } from "./pages/main/main.component";
+import {HomeComponent} from "./pages/home/home.component";
+import {AuctioneerDashboardComponent} from "./pages/auctioneer-dashboard/auctioneer-dashboard.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: MainComponent,
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'auctioneer-dashboard',
+    component: AuctioneerDashboardComponent
+  }
+  ]
